6585de5fba53fccb3daf547168bf525f61241a73
Vladimir-112358 <Vaganov11235813213455@gmail.com>
2024-01-12 00:53:55 +0300

    added some components

---
+++ src/app/components/auth-window/auth-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-window.component.html',
+  styleUrl: './auth-window.component.scss'
+})
+export class AuthWindowComponent {
+
+}
